d20c66c64a6b27d53e2d6be163c9b737f5689671
Dorji Khandu <dorji.khandu@selise.ch>
2022-10-24 18:39:23 +0600

    Update: Model Spec

--- spec/fielder/model/prime_model_spec.rb
+++ spec/fielder/model/prime_model_spec.rb
@@ -4,15 +4,10 @@ require "spec_helper"
 
 RSpec.describe PrimeModel, type: :model do
   describe "#create" do
-    context "test data_cleaning"do
+    context "test data_cleaning" do
       it "creates a prime model" do
         prime_model = PrimeModel.create(name: "Quote", code: "code")
         expect(prime_model).not_to be_nil
-      end
-
-      it "creates another prime model" do
-        prime_model_a= PrimeModel.create(name: "Quote", code: "code")
-        expect(prime_model_a).not_to be_nil
       end
     end
   end
